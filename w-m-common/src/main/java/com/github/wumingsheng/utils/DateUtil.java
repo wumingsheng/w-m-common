30023ebe96c8e64b659e65c3f2c88e7dcf89af29
wumingsheng <mingsheng.wu@qingclass.com>
2019-07-22 11:49:45 +0800

    update datetutil

--- w-m-common/src/main/java/com/github/wumingsheng/utils/DateUtil.java
+++ w-m-common/src/main/java/com/github/wumingsheng/utils/DateUtil.java
@@ -15,18 +15,24 @@ public class DateUtil {
 	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	
 	//date转字符串
-	public static String format(Date date) {
+	public static String format(Date date, String format) {
 		if(null == date) {
 			return null;
+		}
+		if(StringUtils.isNotBlank(format)) {
+			return DateTimeFormatter.ofPattern(format).format(date.toInstant().atZone(ZoneId.of("Asia/Shanghai")).toLocalDateTime());
 		}
 		return DATE_FORMATTER.format(date.toInstant().atZone(ZoneId.of("Asia/Shanghai")).toLocalDateTime());
 	}
 	//字符串转date
-	public static Date parse(String date) {
+	public static Date parse(String date, String format) {
 		if(StringUtils.isBlank(date)) {
 			return null;
 		}
 		LocalDateTime localDateTime = LocalDateTime.parse(date, DATE_FORMATTER);
+		if(StringUtils.isNotBlank(format)) {
+			localDateTime = LocalDateTime.parse(date, DateTimeFormatter.ofPattern(format));
+		}
 		ZoneId zoneId = ZoneId.of("Asia/Shanghai");
         ZonedDateTime zdt = localDateTime.atZone(zoneId);
         return Date.from(zdt.toInstant());
